a4a54033e1673f1df9a7fc9939010ef3ecc9cfba
Akutagawa50 <ryusuke0525a@gmail.com>
2021-05-26 01:00:45 +0900

    変数をclassにまとめた

--- main.cpp
+++ main.cpp
@@ -1,59 +1,95 @@
-#define debug 1
+#define DEBUG 0
+/*-------------*\
+if (DEBUG)
+    cout << "debug" << endl;
+\--------------*/
 
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <vector>
 #include <math.h>
 
-std::string temp_fname[100];  //テンプレートのファイル名[データ番号]
-std::string input_fname[100]; //入力データのファイル名語[データ番号]
+using namespace std;
+/*
+string temp[i].filename0];  //テンプレートのファイル名[データ番号]
+string input[10.filename0]; //入力データのファイル名語[データ番号]
 
-std::string temp_word[100];  //テンプレートの語[データ番号]
-std::string input_word[100]; //入力データの語[データ番号]
+string temp[10.word0];  //テンプレートの語[データ番号]
+string input[10.word0]; //入力データの語[データ番号]
 
-int temp_frame[100] = {0};  //テンプレートのフレーム数[データ番号]
-int input_frame[100] = {0}; //入力データのフレーム数[データ番号]
+int temp[10.frame0] = {0};  //テンプレートのフレーム数[データ番号]
+int input[10.frame0] = {0}; //入力データのフレーム数[データ番号]
 
 const int max_frames = 140;
 
 double temp_data[max_frames][15][100] = {0.0};  //テンプレートを格納する配列[フレーム][次元][データ番号]
 double input_data[max_frames][15][100] = {0.0}; //入力データを格納する配列[フレーム][次元][データ番号]
+*/
 
-double cumulative_dist[max_frames][max_frames] = {0}; //DPマッチングの累積距離を格納する[縦フレーム数][横フレーム数]
+vector<vector<double>> cumulative_dist(10, vector<double>(10)); //DPマッチングの累積距離を格納する
 
-double cumulative_sum[100] = {0}; //認識率[入力データ番号]
+//vector<int> cumulative_sum(100,0); //認識率[入力データ番号]
+double cumulative_sum[100] = {0};
 
-int reco_number[100] = {0}; //一致したテンプレートの番号を格納
+//vector<int> reco_number(100,0); //一致したテンプレートの番号を格納
+int reco_number[100] = {0};
 
 int reco_count = 0; //正答回数を格納
 
+class data_info
+{ //データの情報をまとめるクラス
+public:
+    data_info(){
+        //pass
+    };
+    //~data_info();
+    string filename;             //ファイルの名前
+    string word;                 //単語
+    int frame;                   //フレーム数
+    vector<vector<double>> data; //データ
+};
+
+//テンプレートデータと入力データのクラスをそれぞれ100個宣言
+vector<data_info> temp(100);
+vector<data_info> input(100);
+
 int main()
 {
+    if (DEBUG)
+        cout << "main" << endl;
 
     //テンプレートのデータ入力, city011
     for (int i = 0; i < 100; i++)
     {
-        std::string dir; //番号をcahr型で格納
+        string dir; //番号をcahr型で格納
 
         dir = char(((i + 1) / 100) + '0');              //100の位
         dir += char((((i + 1) % 100) / 10) + '0');      //10の位
         dir += char((((i + 1) % 100) % 10) + '0');      //1の位
         dir = "./data/city011/city011_" + dir + ".txt"; //ディレクトリを代入
 
-        std::ifstream temp_file(dir);
+        ifstream temp_file(dir);
+
+        if (DEBUG)
+            cout << "read template file " << i << " times" << endl;
+
         if (temp_file.fail()) //ファイル読み込みに失敗
         {
-            std::cerr << "Failed to open file." << std::endl;
+            cerr << "Failed to open file." << endl;
             return -1;
         }
-        temp_file >> temp_fname[i]; //ファイル名を入力
-        temp_file >> temp_word[i];  //単語を入力
-        temp_file >> temp_frame[i]; //フレーム数を入力
+        temp_file >> temp[i].filename; //ファイル名を入力
+        temp_file >> temp[i].word;     //単語を入力
+        temp_file >> temp[i].frame;    //フレーム数を入力
 
-        for (int j = 0; j < temp_frame[i]; j++)
+        temp[i].data.assign(temp[i].frame, vector<double>(15, 0)); //2次元配列を動的確保
+
+        for (int j = 0; j < temp[i].frame; j++)
         {
             for (int k = 0; k < 15; k++)
-                temp_file >> temp_data[j][k][i]; //データを入力
+                temp_file >> temp[i].data[j][k]; //データを入力
         }
         temp_file.close(); //ファイルを閉じる
     }
@@ -61,28 +97,35 @@ int main()
     //入力データ入力, city021
     for (int i = 0; i < 100; i++)
     {
-        std::string dir; //番号をcahr型で格納
+        string dir; //番号をcahr型で格納
 
         dir = char(((i + 1) / 100) + '0');              //100の位
         dir += char((((i + 1) % 100) / 10) + '0');      //10の位
         dir += char((((i + 1) % 100) % 10) + '0');      //1の位
         dir = "./data/city021/city021_" + dir + ".txt"; //ディレクトリを代入
 
-        std::ifstream input_file(dir);
+        ifstream input_file(dir);
+
+        if (DEBUG)
+            cout << "read input file " << i << " times" << endl;
+
         if (input_file.fail()) //ファイル読み込みに失敗
         {
-            std::cerr << "Failed to open file." << std::endl;
+            cerr << "Failed to open file." << endl;
             return -1;
         }
-        input_file >> input_fname[i]; //ファイル名を入力
-        input_file >> input_word[i];  //単語を入力
-        input_file >> input_frame[i]; //フレーム数を入力
+        input_file >> input[i].filename; //ファイル名を入力
+        input_file >> input[i].word;     //単語を入力
+        input_file >> input[i].frame;    //フレーム数を入力
 
-        for (int j = 0; j < input_frame[i]; j++)
+        input[i].data.assign(input[i].frame, vector<double>(15, 0)); //2次元配列を動的確保
+
+        for (int j = 0; j < input[i].frame; j++)
         {
             for (int k = 0; k < 15; k++)
             {
-                input_file >> input_data[j][k][i]; //データを入力
+                input_file >> input[i].data[j][k];
+                ; //データを入力
             }
         }
         input_file.close(); //ファイルを閉じる
@@ -92,22 +135,23 @@ int main()
     for (int x = 0; x < 100; x++)
     { //入力データのfor
         for (int y = 0; y < 100; y++)
-        { //テンプレートのfor
-
-            for (int i = 0; i < temp_frame[y]; i++)
+        {                                                                             //テンプレートのfor
+            cumulative_dist.assign(temp[y].frame, vector<double>(input[x].frame, 0)); //2次元配列を動的確保, tempフレーム行 X inputフレーム列
+            for (int i = 0; i < temp[y].frame; i++)
             { //累積距離を計算
 
-                for (int j = 0; j < input_frame[x]; j++)
+                for (int j = 0; j < input[x].frame; j++)
                 {
                     double sum = 0.0;
 
                     //次元の差の2乗和を合計を格納
                     for (int k = 0; k < 15; k++) //次元の差の2乗和を合計する
-                        sum += pow((temp_data[i][k][y] - input_data[j][k][x]), 2.0);
+                        sum += pow((temp[y].data[i][k] - input[x].data[j][k]), 2.0);
 
                     double local_dist = sqrt(sum);
+
                     if (i == 0 && j == 0) //最初はそのまま代入
-                        cumulative_dist[i][j] = local_dist;
+                        cumulative_dist[0][0] = local_dist;
 
                     else if (i == 0) //端は例外処理
                         cumulative_dist[0][j] = cumulative_dist[0][j - 1] + local_dist;
@@ -117,24 +161,18 @@ int main()
 
                     else
                     { //累積距離を上左斜めそれぞれ計算
-                        double up, left, dia, local_min;
+                        double up, left, dia;
                         up = cumulative_dist[i - 1][j] + local_dist;            //上方向の累積
                         left = cumulative_dist[i][j - 1] + local_dist;          //左方向の累積
                         dia = cumulative_dist[i - 1][j - 1] + 2.0 * local_dist; //斜め方向の累積
 
-                        local_min = up;       //上方向を最小値に設定
-                        if (left < local_min) //左方向と大小比較
-                            local_min = left;
-                        if (dia < local_min) //斜め方向と大小比較
-                            local_min = dia;
-
-                        cumulative_dist[i][j] = local_min; //最小値を累積距離に代入
+                        cumulative_dist[i][j] = min({up, left, dia}); //最小値を累積距離に代入
                     }
                 }
             }
             //一致率を計算
             double reco;
-            reco = cumulative_dist[temp_frame[y] - 1][input_frame[x] - 1] / double(input_frame[x] + temp_frame[y]);
+            reco = cumulative_dist[temp[y].frame - 1][input[x].frame - 1] / double(input[x].frame + temp[y].frame);
 
             if (reco < cumulative_sum[x] || y == 0) //最小の累積距離を更新, 1回目はそのまま代入
             {
@@ -146,16 +184,16 @@ int main()
         if (x == reco_number[x])
         { //合ってたら正答数を増やす
             reco_count++;
-            std::cout << "O: "; //〇を出力
+            cout << "O: "; //〇を出力
         }
         else
         {
-            std::cout << "X: "; //間違っていたら×を出力
+            cout << "X: "; //間違っていたら×を出力
         }
 
         //結果を出力
-        std::cout << "resutl of " + input_word[x] + ": " << temp_word[reco_number[x]] << ", Cumulative distance: " << cumulative_sum[x] << std::endl;
+        cout << "resutl of " + input[x].word + ": " << temp[reco_number[x]].word << ", Cumulative distance: " << cumulative_sum[x] << endl;
     }
-    std::cout << "Recognition rate: " << reco_count << "%" << std::endl;
+    cout << "Recognition rate: " << reco_count << "%" << endl;
     return 0;
 }
